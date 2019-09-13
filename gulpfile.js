51f49300b95b1fb034f2d3a5c369ef19a9399334
Roman Vokhmin <vohmin77@gmail.com>
2019-09-13 10:29:24 +0300

    подправлен адаптив

--- gulpfile.js
+++ gulpfile.js
@@ -106,9 +106,9 @@ task('server', () => {
 });
 
 task('watch', () => {
-  watch(`${SRC_PATH}/css/**/*.scss`, series('styles'));
+  watch(`${SRC_PATH}/styles/**/*.scss`, series('styles'));
   watch(`${SRC_PATH}/*.html`, series('copy:html'));
-  watch('./js/*.js', series('scripts'));
+  watch('./scripts/*.js', series('scripts'));
   watch(`${DIST_PATH}/img/icons/*.svg`, series('icons'));
 })
 
